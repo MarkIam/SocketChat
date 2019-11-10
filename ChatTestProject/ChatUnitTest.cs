358e7575216eed953b8a8148e2365aaf12894c34
MarkIam <permarkusss@gmail.com>
2019-11-11 01:29:10 +0500

    Integration test modified.

--- ChatTestProject/ChatUnitTest.cs
+++ ChatTestProject/ChatUnitTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Configuration;
+using System.Threading;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SocketClient.Classes;
 using SocketServer.Classes;
@@ -54,7 +55,7 @@ namespace ChatTestProject
             cmd.TryParse("register", false, out validationMessage);
             Assert.AreEqual(validationMessage, ChatCommand.msgWrongParametersNumber);
 
-            cmd.TryParse("register Mark", false, out validationMessage);
+            cmd.TryParse("register user1", false, out validationMessage);
             Assert.AreEqual(cmd.Type, CommandType.register);
 
             cmd.TryParse("listusers", false, out validationMessage);
@@ -63,9 +64,9 @@ namespace ChatTestProject
             cmd.TryParse("message Hello", false, out validationMessage);
             Assert.AreEqual(validationMessage, ChatCommand.msgWrongParametersNumber);
 
-            cmd.TryParse("message Olya Hello", false, out validationMessage);
+            cmd.TryParse("message user2 Hello", false, out validationMessage);
             Assert.AreEqual(cmd.Type, CommandType.message);
-            Assert.IsTrue(cmd.Arguments["Message"] == "Hello" && cmd.Arguments["RecipientName"] == "Olya");
+            Assert.IsTrue(cmd.Arguments["Message"] == "Hello" && cmd.Arguments["RecipientName"] == "user2");
 
             cmd.TryParse("help someparameter", false, out validationMessage);
             Assert.AreEqual(validationMessage, ChatCommand.msgWrongParametersNumber);
@@ -79,6 +80,7 @@ namespace ChatTestProject
         [TestMethod]
         public void TestIntegration()
         {
+            var timeout = 1000;
             ReadSettings();
 
             var srv = new ChatServer(udpServerPort, tcpServerPort, backlogSize, serverIpAddress);
@@ -86,41 +88,47 @@ namespace ChatTestProject
             Assert.IsTrue(srv.LaunchChatServer());
 
             var clnt1 = new ChatClient(isServerKnown, serverAddress, serverPort);
-
             //clnt1.OnMessage += (msg) => { messageFromServer = msg; };
 
             Assert.IsTrue(clnt1.ConnectToServer());
 
             var validationMessage = string.Empty;
             var command1 = new ChatCommand();
-            Assert.IsTrue(command1.TryParse("register mark", false, out validationMessage));
+            Assert.IsTrue(command1.TryParse("register user1", false, out validationMessage));
+
             clnt1.SendCommand(command1);
+            Thread.Sleep(timeout);
+            Assert.IsTrue(clnt1.LastMessageReceivedFromServer == ChatCommand.msgYouAreSuccessfullyRegistered);
 
             Assert.IsTrue(command1.TryParse("listusers", false, out validationMessage));
             clnt1.SendCommand(command1);
-
-            Assert.IsTrue(clnt1.LastMessageReceivedFromServer == "Подключенные пользователи: mark");
+            Thread.Sleep(timeout);
+            Assert.IsTrue(clnt1.LastMessageReceivedFromServer == "Подключенные пользователи: user1");
 
             var clnt2 = new ChatClient(isServerKnown, serverAddress, serverPort);
-            Assert.IsTrue(clnt1.ConnectToServer());
+            Assert.IsTrue(clnt2.ConnectToServer());
 
             var command2 = new ChatCommand();
-            Assert.IsTrue(command2.TryParse("register olya", false, out validationMessage));
+            Assert.IsTrue(command2.TryParse("register user2", false, out validationMessage));
+
             clnt2.SendCommand(command2);
+            Thread.Sleep(timeout);
+            Assert.IsTrue(clnt2.LastMessageReceivedFromServer == ChatCommand.msgYouAreSuccessfullyRegistered);
 
             Assert.IsTrue(command2.TryParse("listusers", false, out validationMessage));
             clnt2.SendCommand(command2);
+            Thread.Sleep(timeout);
+            Assert.IsTrue(clnt2.LastMessageReceivedFromServer == "Подключенные пользователи: user1, user2");
 
-            Assert.IsTrue(clnt1.LastMessageReceivedFromServer == "Подключенные пользователи: mark, olya");
-
-            Assert.IsTrue(command1.TryParse("message olya 123", false, out validationMessage));
+            Assert.IsTrue(command1.TryParse("message user2 123", false, out validationMessage));
             clnt1.SendCommand(command1);
+            Thread.Sleep(timeout);
+            Assert.IsTrue(clnt2.LastMessageReceivedFromServer == "Пользователь 'user1' прислал вам сообщение: '123'");
 
-
-            Assert.IsTrue(command2.TryParse("message mark 456", false, out validationMessage));
+            Assert.IsTrue(command2.TryParse("message user1 456", false, out validationMessage));
             clnt2.SendCommand(command2);
-
-            Assert.IsTrue(clnt1.LastMessageReceivedFromServer == "456");
+            Thread.Sleep(timeout);
+            Assert.IsTrue(clnt1.LastMessageReceivedFromServer == "Пользователь 'user2' прислал вам сообщение: '456'");
 
             srv.ShutDown();
         }
